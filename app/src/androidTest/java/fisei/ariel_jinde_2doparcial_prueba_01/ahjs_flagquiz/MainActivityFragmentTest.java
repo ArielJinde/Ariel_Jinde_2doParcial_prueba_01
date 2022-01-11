9693c0b114e837744b0108427b67f5bd95f303b3
Ariel <ajinde3033@uta.edu.ec>
2022-01-10 20:24:48 -0500

    commit 20h15

---
+++ app/src/androidTest/java/fisei/ariel_jinde_2doparcial_prueba_01/ahjs_flagquiz/MainActivityFragmentTest.java
@@ -0,0 +1,7 @@
+package fisei.ariel_jinde_2doparcial_prueba_01.ahjs_flagquiz;
+
+import junit.framework.TestCase;
+
+public class MainActivityFragmentTest extends TestCase {
+
+}
---
+++ app/src/main/java/fisei/ariel_jinde_2doparcial_prueba_01/ahjs_flagquiz/AHJSMainActivity.java
@@ -0,0 +1,16 @@
+package fisei.ariel_jinde_2doparcial_prueba_01.ahjs_flagquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import fisei.ariel_jinde_2doparcial_prueba_01.R;
+
+public class AHJSMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ahjsmain);
+    }
+}
